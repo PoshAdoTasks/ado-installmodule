2e8a69ac33802a7f3510263f9738dbff508bb026
Jeffrey Patton <jeffrey@patton-tech.com>
2022-12-29 17:15:30 -0600

    Version 1.1.0 Fix for file-not-found and updated UI

---
+++ CHANGELOG.md
@@ -0,0 +1,26 @@
+# Changelog
+
+All changes to this module should be reflected in this document.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [[1.1.0]](https://github.com/PoshAdoTasks/ado-installmodule/releases/tag/v1.1.0) - 2022-12-29
+
+This update includes a fix for the file not found error as well as a UI update for Scope
+
+The following changes have been applied:
+
+- ps_modules
+  - removed from .gitignore
+- task.json
+  - Scope Property
+    - Changed from String to Picklist
+
+--
+
+## [[1.0.0]](https://github.com/PoshAdoTasks/ado-installmodule/releases/tag/v1.0.0) - 2022-12-29
+
+This is the initial release of the Install Module task. It will allow you to install a powershell module from a task in your Azure DevOps pipelines.
+
+--
--- psakefile.ps1
+++ psakefile.ps1
@@ -1,7 +1,12 @@
 $script:WorkingDir = $PSScriptRoot;
-$script:TaskName = 'adoinstallmodule'
+$script:GithubRepo = 'ado-installmodule'
+$script:TaskName = $script:GithubRepo.Replace('-','')
 $script:Description = 'An Azure DevOps PowerShell Task to install a module'
 $script:Author = 'Jeffrey S. Patton'
+$script:GithubUrl = "https://github.com/PoshAdoTasks/$($script:GithubRepo)"
+$script:MarketplaceUrl = "https://marketplace.visualstudio.com/items?itemName=pattontech.$($script:TaskName)"
+
+Task CreateAdoTask -depends Clean, AddVstsTaskSdk
 
 Task clean {
  Remove-Item "$($script:WorkingDir)\Output" -Recurse -ErrorAction Ignore;
@@ -22,4 +27,17 @@ Task AddVstsTaskSdk -depends SetupTask {
 
 Task CreatePackage -depends Clean {
  tfx extension create --manifest-globs vss-extension.json --output-path "$($script:WorkingDir)\Output"
+}
+
+Task NewTaggedRelease -Description "Create a tagged release" -Action {
+ $Version = Get-Content "$($script:WorkingDir)\vss-extension.json" |ConvertFrom-Json |Select-Object -ExpandProperty Version
+ git tag -a v$version -m "$($script:TaskName) Version $($Version)"
+ git push origin v$version
+}
+
+Task Post2Discord -Description "Post a message to discord" -Action {
+ $version = Get-Content "$($script:WorkingDir)\vss-extension.json" |ConvertFrom-Json |Select-Object -ExpandProperty Version
+ $Discord = Get-Content .\discord.poshmongo | ConvertFrom-Json
+ $Discord.message.content = "Version $($version) of $($script:TaskName) released. Please visit Github ($script:GithubUrl) or the MarketPlace ($script:MarketplaceUrl) to download."
+ Invoke-RestMethod -Uri $Discord.uri -Body ($Discord.message | ConvertTo-Json -Compress) -Method Post -ContentType 'application/json; charset=UTF-8'
 }
